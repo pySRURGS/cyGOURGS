2d62d7aec56fdd59ea125ad4194bf4bfd1f625d3
pySRURGS <pySRURGS@gmail.com>
2020-03-23 12:17:17 -0700

    getting it to work for c++11

--- examples/cybuild.sh
+++ examples/cybuild.sh
@@ -1,1 +1,2 @@
+# Tested on Python 3.6
 python setup.py build_ext --inplace
--- examples/setup.py
+++ examples/setup.py
@@ -6,11 +6,13 @@ import numpy
 extensions = [
     Extension('cython_call', ['cython_call.pyx', '../enumerator.cpp', '../primitiveset.cpp'],
               include_dirs=[numpy.get_include(), 'c:/boost/boost_1_72_0', '..'],
-              language='c++'
+              language='c++',
+              extra_compile_args=["-std=c++11"],
+              extra_link_args=["-std=c++11"]
               ),
 ]
 
 setup(
-    ext_modules=cythonize(extensions, compiler_directives={'language_level' : "3"}),
+    ext_modules=cythonize(extensions, compiler_directives={'language_level' : "3"})
     #extra_compile_args=["-w", '-g'],
 )
