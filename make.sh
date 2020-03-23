63f5d4a2005cab0251e1a2970a989719e12a2501
pySRURGS <pySRURGS@gmail.com>
2020-03-23 11:27:00 -0700

    assigned standard 2011 to makefile

--- make.sh
+++ make.sh
@@ -1,2 +1,2 @@
 path_to_boost=/c/boost/boost_1_72_0
-g++ -I $path_to_boost -I . enumerator.cpp primitiveset.cpp main.cpp -o main.exe
+g++ -std=c++11 -I $path_to_boost -I . enumerator.cpp primitiveset.cpp main.cpp -o main.exe
