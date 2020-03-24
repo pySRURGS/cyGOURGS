54929831f3d7e84e5f0a383a6c51425b079f6294
pySRURGS <sohrabtowfighi@gmail.com>
2020-03-24 16:26:46 -0700

    reorganizing

--- primitiveset.cpp
+++ primitiveset.cpp
@@ -10,6 +10,20 @@ PrimitiveSet::PrimitiveSet()
 
 void PrimitiveSet::add_operator(const string& funct_name, int arity)
 {
+//    A method that adds a user-specified operator to the list of operators
+//    stored in self._operators.
+
+//    Parameters
+//    ----------
+//    func_name : string
+//        The name of a function which will be used in the list of operators.
+
+//    arity : integer
+//        The number of inputs of the function `func_handle`
+
+//    Returns
+//    -------
+//    None
     if (arity < 1)
     {
         std::cerr << "Invalid arity. Must be >= 1.";
@@ -20,16 +34,51 @@ void PrimitiveSet::add_operator(const string& funct_name, int arity)
 
 void PrimitiveSet::add_variable(const string& variable)
 {
+//    A method that adds a user-specified variable to the list of terminals
+//    stored in self._variables.
+
+//    Parameters
+//    ----------
+//    variable: str
+//        The variable or value which will be used as a terminal. Its type
+//        can be anything, but the operators will need to be able to take
+//        `variable` as an input. Within pyGOURGS, it is treated as a string,
+//        but will eventually be evaluated to whatever results from
+//        `eval(variable)`.
+
+//    Returns
+//    -------
+//    None
     m_variables.push_back(variable);
 }
 
 void PrimitiveSet::add_fitting_parameter(const std::string& fitting_parameter)
-{
+{    
+//    A method that adds a fitting parameter to the list of terminals
+//    stored in self._fitting_parameters.
+
+//    Parameters
+//    ----------
+//    param_name : string
+//        The name of the fitting parameter which acts as a terminal
+
+//    Returns
+//    -------
+//    None
     m_fitting_parameters.push_back(fitting_parameter);
 }
 
 std::vector<std::string> PrimitiveSet::get_terminals()
 {
+//    A method that returns the fitting parameters and variables as one list.
+
+//    Parameters
+//    ----------
+//    None
+
+//    Returns
+//    -------
+//    terminals : list (vector)
     vector<string> terminals;
     terminals.reserve(m_fitting_parameters.size() + m_variables.size());
     terminals.insert(terminals.end(), m_fitting_parameters.begin(),
@@ -41,9 +90,15 @@ std::vector<std::string> PrimitiveSet::get_terminals()
 
 std::vector<int> PrimitiveSet::get_arities()
 {
-    /*
-    A method that returns the arities permissible in this search.
-    */
+//    A method that returns the arities permissible in this search.
+
+//    Parameters
+//    ----------
+//    None
+
+//    Returns
+//    -------
+//    arities : a sorted list of integers
     vector<int> arities;
     arities = extract_keys(m_operators_map);
     return arities;
--- primitiveset.h
+++ primitiveset.h
@@ -16,85 +16,20 @@ class PrimitiveSet
 public:
     PrimitiveSet();
 
-//    A method that adds a user-specified operator to the list of operators
-//    stored in self._operators.
-
-//    Parameters
-//    ----------
-//    func_name : string
-//        The name of a function which will be used in the list of operators.
-
-//    arity : integer
-//        The number of inputs of the function `func_handle`
-
-//    Returns
-//    -------
-//    None
     void add_operator(const std::string& funct_name, int arity);
-
-//    A method that adds a user-specified variable to the list of terminals
-//    stored in self._variables.
-
-//    Parameters
-//    ----------
-//    variable: str
-//        The variable or value which will be used as a terminal. Its type
-//        can be anything, but the operators will need to be able to take
-//        `variable` as an input. Within pyGOURGS, it is treated as a string,
-//        but will eventually be evaluated to whatever results from
-//        `eval(variable)`.
-
-//    Returns
-//    -------
-//    None
     void add_variable(const std::string& variable);
-
-//    A method that adds a fitting parameter to the list of terminals
-//    stored in self._fitting_parameters.
-
-//    Parameters
-//    ----------
-//    param_name : string
-//        The name of the fitting parameter which acts as a terminal
-
-//    Returns
-//    -------
-//    None
     void add_fitting_parameter( const std::string& fitting_parameter );
-
-
-//    A method that returns the fitting parameters and variables as one list.
-
-//    Parameters
-//    ----------
-//    None
-
-//    Returns
-//    -------
-//    terminals : list (vector)
     std::vector<std::string> get_terminals();
-
-//    A method that returns the arities permissible in this search.
-
-//    Parameters
-//    ----------
-//    None
-
-//    Returns
-//    -------
-//    arities : a sorted list of integers
     std::vector<int> get_arities();
-
     std::vector<std::string> get_operators(int arity);
+    std::vector<std::string> m_variables;
+    std::vector<std::string> m_fitting_parameters;
+    std::map<int, std::vector<std::string> > m_operators_map;
+    
 private:
 
     std::vector<int> extract_keys(
                      std::map<int, std::vector<std::string> > const& input_map);
-
-private:
-    std::vector<std::string> m_variables;
-    std::vector<std::string> m_fitting_parameters;
-    std::map<int, std::vector<std::string> > m_operators_map;
     std::vector<std::string> m_names;
 };
 
