440a92a3eed07491f53d004682be2e690607aadc
pySRURGS <sohrabtowfighi@gmail.com>
2020-03-24 16:27:09 -0700

    removed extraneous spaces

--- primitiveset.h
+++ primitiveset.h
@@ -15,7 +15,6 @@ class PrimitiveSet
 {
 public:
     PrimitiveSet();
-
     void add_operator(const std::string& funct_name, int arity);
     void add_variable(const std::string& variable);
     void add_fitting_parameter( const std::string& fitting_parameter );
@@ -27,7 +26,6 @@ public:
     std::map<int, std::vector<std::string> > m_operators_map;
     
 private:
-
     std::vector<int> extract_keys(
                      std::map<int, std::vector<std::string> > const& input_map);
     std::vector<std::string> m_names;
