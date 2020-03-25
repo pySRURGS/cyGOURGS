3676668e22f7770f1edd548b993f5d0ae708a172
pySRURGS <sohrabtowfighi@gmail.com>
2020-03-25 14:36:17 -0700

    ensuring works across platforms

--- make.sh
+++ make.sh
@@ -1,11 +1,16 @@
 # compile the main.exe, demonstrating that codes of core functionality compile
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    : # do nothing
+    # assuming that user has already installed libboost-all-dev
+    g++ -std=c++11 -I . enumerator.cpp primitiveset.cpp \
+        main.cpp -o main.exe
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     path_to_boost=/c/boost/boost_1_72_0
+    g++ -std=c++11 -I $path_to_boost -I . enumerator.cpp primitiveset.cpp \
+        main.cpp -o main.exe
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
     path_to_boost=/c/boost/boost_1_72_0
+    g++ -std=c++11 -I $path_to_boost -I . enumerator.cpp primitiveset.cpp \
+        main.cpp -o main.exe
 fi
 
-g++ -std=c++11 -I $path_to_boost -I . enumerator.cpp primitiveset.cpp main.cpp \
-    -o main.exe
+
