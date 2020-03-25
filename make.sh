958836f28b112adbe929ec26898aee0160bb1e72
pySRURGS <sohrabtowfighi@gmail.com>
2020-03-25 14:34:30 -0700

    assuming that linux has boost already installed

--- make.sh
+++ make.sh
@@ -1,2 +1,11 @@
-path_to_boost=/c/boost/boost_1_72_0
-g++ -std=c++11 -I $path_to_boost -I . enumerator.cpp primitiveset.cpp main.cpp -o main.exe
+# compile the main.exe, demonstrating that codes of core functionality compile
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    : # do nothing
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    path_to_boost=/c/boost/boost_1_72_0
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    path_to_boost=/c/boost/boost_1_72_0
+fi
+
+g++ -std=c++11 -I $path_to_boost -I . enumerator.cpp primitiveset.cpp main.cpp \
+    -o main.exe
