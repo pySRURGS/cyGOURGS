fccb5be928e1525452f52e252d581c0dfcb581c3
pySRURGS <sohrabtowfighi@gmail.com>
2020-03-28 10:42:52 -0700

    dded instsall commans for boosts

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 # download the boost c++ package
-wget -O ~/boost_1_72_0.tar.gz https://dl.bintray.com/boostorg/release/1.72.0/source/boost_1_72_0.tar.gz --no-check-certificate
+current_dir=$pwd
+wget -O ~/boost_1_72_0.tar.gz http://downloads.sourceforge.net/project/boost/boost/1.72.0/boost_1_72_0.tar.gz --no-check-certificate
 tar  -C ~ -zxf ~/boost_1_72_0.tar.gz 
+cd boost_1_72_0
+./bootstrap.sh
+./b2 install
+cd $current_dir
 # compile the main.exe, demonstrating that codes of core functionality compile
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     path_to_boost=~/boost_1_72_0
