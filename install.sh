45b26263af6189213d66c883a91e5216867fbaf1
pySRURGS <sohrabtowfighi@gmail.com>
2020-03-28 09:33:23 -0700

    added boost path to install linux

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ tar  -C ~ -zxf ~/boost_1_72_0.tar.gz
 # compile the main.exe, demonstrating that codes of core functionality compile
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     path_to_boost=~/boost_1_72_0
-    g++ -std=c++11 -I . enumerator.cpp primitiveset.cpp \
+    g++ -std=c++11 -I $path_to_boost -I . enumerator.cpp primitiveset.cpp \
         main.cpp -o main.exe
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
     path_to_boost=/c/boost/boost_1_72_0
