fc80cf03b863f9313788fef38e41501c9c698500
pySRURGS <sohrabtowfighi@gmail.com>
2020-03-28 08:45:44 -0700

    consolidating installation

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
+# download the boost c++ package
+cd ~
+wget http://downloads.sourceforge.net/project/boost/boost/1.72.0/boost_1_72_0.tar.gz
+tar -zxvf boost_1_72_0.tar.gz
 # compile the main.exe, demonstrating that codes of core functionality compile
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    # assuming that user has already installed libboost-all-dev
+    path_to_boost=~/boost_1_72_0
     g++ -std=c++11 -I . enumerator.cpp primitiveset.cpp \
         main.cpp -o main.exe
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
@@ -8,8 +12,6 @@ elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
     g++ -std=c++11 -I $path_to_boost -I . enumerator.cpp primitiveset.cpp \
         main.cpp -o main.exe
 fi
-
-# now compile the cython code
-# Tested on Python 3.6
+# Compile the Cython code
 python setup.py build_ext --inplace
 
