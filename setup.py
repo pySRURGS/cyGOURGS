c2978153e2f67b63b01ca64be6f39d4597894a0e
pySRURGS <sohrabtowfighi@gmail.com>
2020-03-28 09:53:21 -0700

    working on cross platform install for users/travis

--- setup.py
+++ setup.py
@@ -2,17 +2,32 @@ from distutils.core import setup
 from Cython.Build import cythonize
 from distutils.extension import Extension
 import numpy
+import sys
+try:
+    import sh
+except ImportError: # on Windows
+    # fallback: emulate the sh API with pbs
+    import pbs
+
+    class Sh(object):
+        def __getattr__(self, attr):
+            return pbs.Command(attr)
+    sh = Sh()
+
+boost_path = sh.echo('~').strip() + '/boost_1_72_0'
 
 extensions = [
-    Extension('cython_call', ['cython_call.pyx', 'enumerator.cpp', 'primitiveset.cpp'],
-              include_dirs=[numpy.get_include(), 'c:/boost/boost_1_72_0', '.'],
+    Extension('cython_call', ['cython_call.pyx', 
+                              'enumerator.cpp', 
+                              'primitiveset.cpp'],
+              include_dirs=[numpy.get_include(), 
+                            boost_path, 
+                            '.'],
               language='c++',
               extra_compile_args=["-std=c++11"],
               extra_link_args=["-std=c++11"]
               ),
 ]
 
-setup(
-    ext_modules=cythonize(extensions, compiler_directives={'language_level' : "3"})
-    #extra_compile_args=["-w", '-g'],
-)
+setup(ext_modules=cythonize(extensions, 
+                            compiler_directives={'language_level' : "3"}))
